cfb20949035f5b524c6bfc607e0675962550016d
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-04-03 09:11:02 +0300

    task 1.2

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function computeAreaOfRectangle(length, width){
 
 var output = computeAreaOfRectangle(4, 8);
 console.log(output);
+
+//1.2
+function computeAreaOfCircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+var output = computeAreaOfCircle(4);
+console.log(output);
