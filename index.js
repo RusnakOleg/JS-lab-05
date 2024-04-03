cd8826a52b01da1ee54c481fe20a9b97aaee5415
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-04-03 09:26:30 +0300

    task 1.7

--- index.js
+++ index.js
@@ -45,3 +45,27 @@ function joinArrays(array1, array2) {
 
 var output = joinArrays([1, 2], [3, 4]);
 console.log(output);
+
+//1.7
+function getProductOfAllElementsAtProperty(obj, key) {
+    if (!obj.hasOwnProperty(key)) {
+        return 0;
+    }
+
+    if (!Array.isArray(obj[key]) || obj[key].length === 0) {
+        return 0;
+    }
+
+    var product = 1;
+    for (var i = 0; i < obj[key].length; i++) {
+        product *= obj[key][i];
+    }
+
+    return product;
+}
+
+var obj = {
+    key: [1, 2, 3, 4]
+};
+var output = getProductOfAllElementsAtProperty(obj, 'key');
+console.log(output);
