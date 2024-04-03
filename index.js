321858ed351d1776ba918b741c43e5bad58bb915
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-04-03 09:23:34 +0300

    task 1.6

--- index.js
+++ index.js
@@ -37,3 +37,11 @@ function getLengthOfThreeWords(word1, word2, word3) {
 
 var output = getLengthOfThreeWords('some', 'other', 'words');
 console.log(output);
+
+//1.6
+function joinArrays(array1, array2) {
+    return array1.concat(array2);
+}
+
+var output = joinArrays([1, 2], [3, 4]);
+console.log(output);
